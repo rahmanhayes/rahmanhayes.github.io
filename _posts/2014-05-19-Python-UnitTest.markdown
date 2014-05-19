7630ed057fbeaaf4b3b7d12683ddc34347a993ef
rjarmand <rjarmand@yahoo.com>
2014-05-19 12:38:54 -0700

    Corrected syntax issues

--- _posts/2014-05-19-Python-UnitTest.markdown
+++ _posts/2014-05-19-Python-UnitTest.markdown
@@ -33,7 +33,7 @@ class TddInPythonExample(unittest.TestCase):
 
     def test_calculator_add_method_returns_correct_result(self):
         calc = Calculator()
-        result - calc.add(2,2)
+        result = calc.add(2,2)
         self.assertEqual(4, result)
 {% endhighlight %}
 
--- _posts/calculator_tests.py
+++ _posts/calculator_tests.py
@@ -1,12 +1,12 @@
 class Calculator(object):
     def add(self, x, y):
-        pass
+        return x+y
 
 import unittest
 class TddInPythonExample(unittest.TestCase):
     def test_calculator_add_method_returns_correct_result(self):
         calc = Calculator()
-        result - calc.add(2,2)
+        result = calc.add(2,2)
         self.assertEqual(4, result)
 
 if __name__ == "__main__":
